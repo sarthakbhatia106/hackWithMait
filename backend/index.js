7b616d50b37f0740155729458296295aee2e2a90
Harshit <harshitkumar2468@gmail.com>
2020-11-28 15:45:54 +0530

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
-const express= require('express');
-const app= express();
-const OnemgRouter= require("./1mg/router");
-const netMedRouter=require("./netmeds/router");
-const pharmEasyRouter=require("./pharmEasy/router");
+const express = require("express");
+const app = express();
+const OnemgRouter = require("./1mg/router");
+const netMedRouter = require("./netmeds/router");
+const pharmEasyRouter = require("./pharmEasy/router");
 
+app.use("/1mg", OnemgRouter);
+app.use("/netmed", netMedRouter);
+app.use("/pharmeasy", pharmEasyRouter);
 
-app.use("/1mg",OnemgRouter);
-app.use("/netmed",netMedRouter);
-app.use("/pharmeasy",pharmEasyRouter);
-
-app.listen(8000,(req,res)=>{
+app.listen(8000, (req, res) => {
     console.log(`app is running at 8000`);
-})
+});
